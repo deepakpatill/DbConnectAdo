7fbb7b2a0dbb45ba4e06cbf5e55dd6d27753ebcc
Deepak Patil <deepak.patil@atriina.com>
2023-01-24 12:03:50 +0530

    Add project files.

---
+++ DbConnectAdo/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using DbConnectAdo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DbConnectAdo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration configuration;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(IConfiguration configuration, ILogger<HomeController> logger)
+        {
+            this.configuration = configuration;
+            _logger = logger;
+        }
+        
+
+        public IActionResult Index()
+        {
+            string connection = configuration.GetConnectionString("Default");
+            SqlConnection conn = new SqlConnection(connection);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Select count(*) from employee", conn);
+            var count = (int)cmd.ExecuteScalar();
+            ViewData["TotalData"] = count;
+            conn.Close();
+                
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DbConnectAdo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DbConnectAdo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
